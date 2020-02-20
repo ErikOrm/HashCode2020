d4b8a43960e8725104eb3dac9325aaeb0eeda3db
unknown <erohe@dtu.dk>
2020-02-20 20:11:08 +0100

    a

--- erik.py
+++ erik.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import math
 # f = open("a_example.txt", "r")
 # f = open("b_read_on.txt", "r")
 # f = open("c_incunabula.txt", "r")
@@ -33,3 +33,24 @@ def load_data(name):
             library_books[l, b] = True
 
     return (B, L, D, library_books, book_values, n_books, n_days, ship_rate)
+
+
+def get_library_value(B, library_books, library, remaining_time, ship_rate):
+    remaining_books = np.sum(library_books[l,:])
+    prod = numpy.multiply(library_books[l,:], book_values)
+    remaining_books_to_ship = remaining_time*ship_rate
+    if remaining_books_to_ship >= remaining_books:
+        end = B
+    else:
+        LB = 0
+        UB = B
+        while LB != UB:
+            end = math.ceil((UB + LB)/2)
+            if  np.sum(library_books[l,:end]) >= remaining_books_to_ship:
+                UB = end
+            else:
+                LB  = end
+    return np.sum(prod[:end]), math.ceil((remaining_books_to_ship-remaining_books)/ship_rate)
+
+
+def basic_greedy(B, L, D, library_books, book_values, n_books, n_days, ship_rate):
