629b22f78a6ba4948ee4cc8adb700b2190dcd863
Emilio Jorge <emilio@jorge.se>
2020-02-20 20:40:39 +0100

    Add start to algorithm

--- emilio.py
+++ emilio.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def set_up(book_value, library_books):
     # library_books  = Libraries x Books
-    sortindex = np.argsort(book_value)
+    sortindex = np.argsort(-1*book_value)
     gobackindex = np.argsort(sortindex)
     return sortindex,book_value[sortindex],library_books[:, sortindex], gobackindex
 
