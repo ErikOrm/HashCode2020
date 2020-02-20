b4264dd46af10d9096ab6ae252a358e6a1ddaa1c
Emilio Jorge <emilio@jorge.se>
2020-02-20 19:34:15 +0100

    Add transform of books

---
+++ emilio.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def set_up(book_value, library_books):
+    # library_books  = Libraries x Books
+    sortindex = np.argsort(book_value)
+    gobackindex = np.argsort(sortindex)
+    return sortindex,book_value[sortindex],library_books[:, sortindex], gobackindex
+
